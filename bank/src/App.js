d30fb376f62df3b054b625b171cec679c7c9a918
tzlilhaim <tzlilhaim480@gmail.com>
2020-10-07 18:12:28 +0300

    Added snackbar and styling

--- bank/src/App.js
+++ bank/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import axios from "axios"
 import "./styles/App.css"
+import { Snackbar, IconButton } from "@material-ui/core"
 import Transactions from "./components/Transactions"
 import Operations from "./components/Operations"
 import Landing from "./components/Landing"
@@ -12,18 +13,17 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
+      snackBarOpen: false,
+      snackBarMsg: "",
       transactions: [],
     }
   }
-  componentDidMount() {
-    axios
-      .get(`http://localhost:${SERVER_PORT}/transactions`)
-      .then((transactions) => {
-        this.setState({ transactions: transactions.data })
-      })
-      .catch(function (error) {
-        console.log(error)
-      })
+  getTransactions = async () => {
+    return axios.get(`http://localhost:${SERVER_PORT}/transactions`)
+  }
+  async componentDidMount() {
+    const transactions = await this.getTransactions()
+    this.setState({ transactions: transactions.data })
   }
   addTransaction = async (data) => {
     const { amount, vendor, category } = data
@@ -31,7 +31,11 @@ class App extends Component {
     axios
       .post(`http://localhost:${SERVER_PORT}/transaction`, newTransaction)
       .then((updatedTransactions) => {
-        this.setState({ transactions: updatedTransactions.data })
+        this.setState({
+          transactions: updatedTransactions.data,
+          snackBarOpen: true,
+          snackBarMsg: "Added transaction successfully!",
+        })
       })
       .catch(function (error) {
         console.log(error)
@@ -41,16 +45,39 @@ class App extends Component {
     axios
       .delete(`http://localhost:${SERVER_PORT}/transaction/${id}`)
       .then((updatedTransactions) => {
-        this.setState({ transactions: updatedTransactions.data })
+        this.setState({
+          transactions: updatedTransactions.data,
+          snackBarOpen: true,
+          snackBarMsg: "Removed transaction successfully!",
+        })
       })
       .catch(function (error) {
         console.log(error)
       })
   }
+  snackBarClose = (event) => {
+    this.setState({ snackBarOpen: false })
+  }
   render() {
     return (
       <Router>
         <div className="App">
+          <Snackbar
+            open={this.state.snackBarOpen}
+            autoHideDuration={3000}
+            onClose={this.snackBarClose}
+            message={<span id="snackbar-msg">{this.state.snackBarMsg}</span>}
+            action={
+              <IconButton
+                key="close"
+                arial-label="Close"
+                color="inherit"
+                onClick={this.snackBarClose}
+              >
+                x
+              </IconButton>
+            }
+          />
           <div id="main-links">
             <Link to={"/"}>Home</Link>
             <Link to={"/transactions"}>Transactions</Link>
